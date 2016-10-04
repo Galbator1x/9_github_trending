cc337202ac94fbb5cda0ad3c57b7222d0bb663bc
Ivan Filimonov <snake73w@gmail.com>
2016-10-04 16:36:42 +0700

    Refactored get_trending_repositories, added output_repositories_to_console.

--- github_trending.py
+++ github_trending.py
@@ -2,28 +2,27 @@ import requests
 from datetime import date, timedelta
 
 
-def get_trending_repositories():
-    week_ago = date.today() - timedelta(days=7)
-    url = 'https://api.github.com/search/repositories?q=created:%3E{}&sort=stars&order=desc'. \
-        format(str(week_ago.isoformat()))
-    repositories = requests.get(url).json()
-    trending_repos = {}
-    for repo in repositories['items'][:20]:
-        name = repo['name']
-        trending_repos[name] = {'owner': repo['owner']['login'],
-                                'stars': repo['stargazers_count'],
-                                'repo_url': repo['html_url'],
-                                'issues': repo['open_issues_count']}
+REPOSITORIES_COUNT = 20
 
-    return trending_repos
+
+def get_trending_repositories():
+    url = 'https://api.github.com/search/repositories'
+    week_ago = date.today() - timedelta(days=7)
+    payload = {'q': 'created:>{}'.format(str(week_ago.isoformat())),
+               'sort': 'stars', 'order': 'desc'}
+    repositories = requests.get(url, params=payload).json()
+    return repositories['items'][:REPOSITORIES_COUNT]
+
+
+def output_repositories_to_console(repos):
+    print('Trending repositories about this week.\n')
+    for repo in sorted(repos, key=lambda repo: repo['stargazers_count'],
+                       reverse=True):
+        print('stars: {:<5} open issues: {:<3} {} {}'.format(str(repo['stargazers_count']),
+                                                             str(repo['open_issues_count']),
+                                                             repo['name'],
+                                                             str(repo['html_url'])))
 
 
 if __name__ == '__main__':
-    print('Trending repositories about this week.\n')
-    for repo_name, repo in sorted(get_trending_repositories().items(),
-                                  key=lambda repo: repo[1]['stars'],
-                                  reverse=True):
-        print('stars: {:<5} open issues: {:<3} {} {}'.format(str(repo['stars']),
-                                                             str(repo['issues']),
-                                                             repo_name,
-                                                             str(repo['repo_url'])))
+    output_repositories_to_console(get_trending_repositories())
